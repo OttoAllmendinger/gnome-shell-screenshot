519ff2a9173c535956e1507f514443989e1429da
Otto Allmendinger <otto.allmendinger@gmail.com>
2017-05-08 10:23:46 +0200

    Add uploadDummy for testing

---
+++ src/uploadDummy.js
@@ -0,0 +1,32 @@
+const Lang = imports.lang;
+const Signals = imports.signals;
+const Mainloop = imports.mainloop;
+
+const Upload = new Lang.Class({
+  Name: "Upload",
+
+  _init: function (file) {
+  },
+
+  start: function () {
+    const testImage = 'http://i.imgur.com/Vkapy8W.png';
+    const size = 200000;
+    const chunk = 1000;
+    const updateMs = 100;
+    let progress = 0;
+
+    let update = Lang.bind(this, () => {
+      if (progress < size) {
+        this.emit("progress", (progress += chunk), size);
+        Mainloop.timeout_add(updateMs, update);
+      } else {
+        this.responseData = {link: testImage};
+        this.emit("done", this.responseData);
+      }
+    });
+
+    Mainloop.idle_add(update);
+  }
+});
+
+Signals.addSignalMethods(Upload.prototype);
