09e4bf8a65f716af01ed859b489c107669b5838f
Otto Allmendinger <mail@ttll.de>
2019-10-01 16:59:12 +0200

    Fix versionGreater()
    Some things are more complex than they look!
    
    Fixes #100

--- src/version.js
+++ src/version.js
@@ -1,26 +1,27 @@
 const versionArray = (v) => v.split(".").map(Number);
 
-const zip = function(a, b) {
-    const headA = a.shift();
-    const headB = b.shift();
-    if ((headA !== undefined) || (headB !== undefined)) {
-        return [[headA, headB]].concat(zip(a, b));
-    } else {
+const zip = function(a, b, defaultValue) {
+    if (a.length === 0 && b.length === 0) {
         return [];
     }
+    const headA = (a.length > 0) ? a.shift() : defaultValue;
+    const headB = (b.length > 0) ? b.shift() : defaultValue;
+    return [[headA, headB]].concat(zip(a, b, defaultValue));
 };
 
 function versionEqual(a, b) {
-    return zip(versionArray(a), versionArray(b)).reduce(
-        (prev, [a, b], index) => prev && (a === b)
+    return zip(versionArray(a), versionArray(b), 0).reduce(
+        (prev, [a, b]) => prev && (a === b)
     , true);
 }
 
 function versionGreater(a, b) {
-    return (!versionEqual(a, b))
-        && zip(versionArray(a), versionArray(b)).reduce(
-            (prev, [a, b], index) => prev && (a >= (b || 0))
-    , true);
+    const diff = zip(versionArray(a), versionArray(b), 0).find(([a, b]) => a !== b);
+    if (!diff) {
+        return false;
+    }
+    const [x, y] = diff;
+    return x > y;
 }
 
 function versionSmaller(a, b) {
@@ -32,7 +33,30 @@ if (window["ARGV"] && ARGV[0] == "test") {
         versionArray("1.2.3"),
         versionArray("1.2"))));
     log("versionEqual(\"1.2.3\", \"1.2\")=" + versionEqual("1.2.3", "1.2"));
-    log("versionGreater(\"3.10.1\", \"3.10\")=" + versionGreater("3.10.1", "3.10"));
+    [
+      ["1", "1", false],
+      ["1", "1.0", false],
+      ["1", "1.0.0", false],
+      ["1.0", "1.0", false],
+      ["1.2", "2.1", false],
+      ["1.2.3", "2.1", false],
+      ["2.1", "1.2", true],
+      ["2.1.1", "1.2", true],
+      ["1.2.1", "1.2.0", true],
+      ["1.2.1", "1.2", true],
+      ["1.2", "1.2.0", false],
+      ["1.2", "1.2.1", false],
+      ["3.32.2", "3.32", true],
+      ["3.32", "3.32.2", false],
+    ].forEach(([a, b, expected]) => {
+        const actual = versionGreater(a, b);
+        if (expected !== actual) {
+            log(
+                `ERROR: versionGreater("${a}", "${b}") is ${actual}, ` +
+                `expected ${expected}`
+            );
+        }
+    });
 }
 
 var exports = {
