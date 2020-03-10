5305ad6f4f8e5114eafae4ecf0fe8d169ff33fb3
Otto Allmendinger <otto.allmendinger@gmail.com>
2020-03-10 16:59:45 +0100

    Defer instantiation of clipboard
    Apparently `Gdk.Display.get_default()` can be `null` sometimes.

--- src/clipboard.js
+++ src/clipboard.js
@@ -1,14 +1,16 @@
 const Gtk = imports.gi.Gtk;
 const Gdk = imports.gi.Gdk;
 
-const clipboard = Gtk.Clipboard.get_default(Gdk.Display.get_default());
+const getClipboard = function() {
+  return Gtk.Clipboard.get_default(Gdk.Display.get_default());
+}
 
 const setImage = function(gtkImage) {
-    clipboard.set_image(gtkImage.get_pixbuf());
+    getClipboard().set_image(gtkImage.get_pixbuf());
 }
 
 const setText = function(text) {
-    clipboard.set_text(text, -1);
+    getClipboard().set_text(text, -1);
 }
 
 var exports = {
